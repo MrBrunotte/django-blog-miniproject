6d752214e9c72bb132734ae667b1e914d20ba9ea
mrbrunotte <mrbrunotte@gmail.com>
2020-02-29 17:41:34 +0100

    Part one done

--- posts/views.py
+++ posts/views.py
@@ -8,26 +8,29 @@ def get_posts(request):
     """
     Create a view that will return a list of Posts that were published prior to 'now' and render them to the 'blogposts.html' template
     """
-    
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
+
 
 def post_detail(request, pk):
     """
     Create a view that will return a single Post object based on the post ID (pk) and render it to the 'postdetail.html' template. Or return an 404 error if the post is not found.
     """
 
-    post = get_object_or_404(Post,pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     post.views += 1
     post.save()
     return render(request, "postdetail.html", {'post': post})
 
-def create_or_edit_post(request, pk=none):
+
+def create_or_edit_post(request, pk=None):
     """
     create a view that allows us to create or edit a post, depending if the post ID is null or not.
     """
 
-    post = get_object_or_404(Post,pk=pk) if pk else None
+    post = get_object_or_404(Post, pk=pk) if pk else None
     if request.method == "POST":
         form = BlogPostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
